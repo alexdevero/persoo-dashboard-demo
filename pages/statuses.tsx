714a75843bc42d3810f7d17bfc9f31303f31efb8
Alex Devero <deveroalex@gmail.com>
2020-11-10 16:48:07 +0100

    Prepare page for statuses

---
+++ pages/statuses.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+import styles from '../styles/Home.module.css'
+
+export default function Statuses() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Statuses</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="container text-center">
+        <h1 className="h2 mt-0 mb-4">
+          Welcome back commander!
+        </h1>
+
+        <Link href="/"><a className="text-underline">&larr; Back home</a></Link>
+      </main>
+    </div>
+  )
+}
