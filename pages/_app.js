1a4696e72396e08274030abf5aeec34f4db1249a
Alex Devero <deveroalex@gmail.com>
2020-11-10 12:52:57 +0100

    Add new CSS stylesheets

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles/globals.css'
+import './../styles/bootstrap.min.css'
+import './../styles/modern-normalize.css'
+
+import './../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
