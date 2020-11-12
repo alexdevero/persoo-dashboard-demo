2e0f6d71751acdb4b507cb40b3f319fbaf2e67d8
Alex Devero <deveroalex@gmail.com>
2020-11-12 16:40:54 +0100

    Add example of credentials

---
+++ credentials/credentials.example.ts
@@ -0,0 +1,9 @@
+export const cookieToken = ''
+
+export const login = ''
+export const pass = ''
+
+export const persooScenariosEndpoint = ''
+export const persooOffersEndpoint = ''
+export const persooAccountInfoEndpoint = ''
+export const persooUserInfoEndpoint = ''
