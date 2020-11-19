ac168131206f1b5e090c5ac2bc2414fa4a7b2491
Alex Devero <deveroalex@gmail.com>
2020-11-19 10:01:50 +0100

    Add variables and typography stylesheets

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import './../styles/bootstrap.min.css'
 import './../styles/modern-normalize.css'
 
 import './../styles/globals.css'
+import './../styles/variables.css'
+import './../styles/typography.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
